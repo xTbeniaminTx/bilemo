6cfadc7c25215e67664fd6e9e01b4805b8af8525
btolan-karudev <beniamin.tolan@outlook.com>
2020-07-31 18:18:59 +0200

    #7: fixtures separate

---
+++ src/DataFixtures/UserCustomerFixtures.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\AccessProvider;
+use App\Entity\Customer;
+use App\Entity\Product;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture implements DependentFixtureInterface
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+
+            $hash = $this->encoder->encodePassword($user, "password");
+
+            $user->setLogin($faker->userName)
+                ->setPassword($hash);
+
+            $manager->persist($user);
+
+
+            for ($c = 0; $c < mt_rand(5, 15); $c++) {
+                $customer = new Customer();
+                $customer->setFirstName($faker->firstName)
+                    ->setLastName($faker->lastName)
+                    ->setEmail($faker->email)
+                    ->setPassword($faker->password)
+                    ->setUser($user);
+                $this->addReference('Customer-' . $c, $customer);
+                $manager->persist($customer);
+            }
+
+        }
+
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ProductFixtures::class
+        ];
+    }
+}

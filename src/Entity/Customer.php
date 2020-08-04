81cc9f39dbeb80cfd71aa2b54836db44d911c614
btolan-karudev <beniamin.tolan@outlook.com>
2020-08-04 14:39:27 +0200

    #10:apiplatform installed

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -2,13 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CustomerRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ * @ApiResource()
  */
 class Customer
 {
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
+ * @ApiResource()
  */
 class Product
 {

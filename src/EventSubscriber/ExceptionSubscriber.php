365d7e8aebe2f937d86913f4253fbc670f5f7e42
btolan-karudev <beniamin.tolan@outlook.com>
2020-08-04 16:29:37 +0200

    exception code status

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $data = [
+            'status' => $exception->getCode(),
+            'message' => 'Resource not found'
+        ];
+
+        $response = new JsonResponse($data);
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}

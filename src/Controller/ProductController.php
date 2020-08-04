6266fa33bf1d47d356e4f365abfa1def50155255
btolan-karudev <beniamin.tolan@outlook.com>
2020-08-04 16:04:07 +0200

    contriller phone and show phone

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * Class ProductController
+ * @Route("/api/phones")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/{id}", name="show_phone", methods={"GET"})
+     * @param Product $phone
+     * @param ProductRepository $phoneRepository
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function show(Product $phone, ProductRepository $phoneRepository, SerializerInterface $serializer)
+    {
+        $phone = $phoneRepository->find($phone->getId());
+        $data = $serializer->serialize($phone, 'json');
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/{page<\d+>?1}", name="list_phone", methods={"GET"})
+     * @param Request $request
+     * @param ProductRepository $productRepository
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function index(Request $request,ProductRepository $productRepository, SerializerInterface $serializer)
+    {
+        $page = $request->query->get('page');
+        if(is_null($page) || $page < 1) {
+            $page = 1;
+        }
+        $phones = $productRepository->findAllPhones($page, 10);
+        $data = $serializer->serialize($phones, 'json');
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+
+}
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +18,15 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    public function findAllPhones($page, $limit)
+    {
+        $query = $this->createQueryBuilder('p')
+            ->getQuery()
+            ->setFirstResult(($page - 1) * $limit)
+            ->setMaxResults($limit);
+        return new Paginator($query);
     }
 
     // /**

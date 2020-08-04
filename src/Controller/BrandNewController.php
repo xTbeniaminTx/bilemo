3cb0ccf7ecb6e4415631f9fb4112b3987dfc1cf0
btolan-karudev <beniamin.tolan@outlook.com>
2020-08-04 15:14:55 +0200

    test resouce and postman

--- src/Controller/BrandNewController.php
+++ src/Controller/BrandNewController.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class BrandNewController extends AbstractController
 {
     /**
-     * @Route("/brand/new", name="brand_new")
+     * @Route("/api", name="api")
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @return Response
      */
-    public function index()
+    public function index(SerializerInterface $serializer, EntityManagerInterface $entityManager)
     {
-        return $this->json([
-            'name' => 'iPhone X',
-            'price' => 1000
+        $phone = $entityManager->find(Product::class, 31);
+
+        $data = $serializer->serialize($phone, 'json');
+
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
         ]);
     }
 }
--- src/DataFixtures/UserCustomerFixtures.php
+++ src/DataFixtures/UserCustomerFixtures.php
@@ -7,7 +7,7 @@ use App\Entity\Customer;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
